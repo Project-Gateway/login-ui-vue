26d6cc19da68bf91e96f0589bd1690c940cf9e19
jefersonralmeida <jefersonparanaense@gmail.com>
2018-08-29 17:12:47 -0300

    removing env file

--- config/dev.env.js
+++
@@ -1,8 +0,0 @@
-'use strict'
-const merge = require('webpack-merge')
-const prodEnv = require('./prod.env')
-
-module.exports = merge(prodEnv, {
-  NODE_ENV: '"development"',
-  LOGIN_API_URL: '"https://local.pg.com/login-api"',
-});
